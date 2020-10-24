5929f9e94a710479e9310c405ca0406979f00ccc
Fatimah <daviesfatimah97@gmail.com>
2020-10-24 05:42:06 -0700

    Updates

---
+++ precache-manifest.09a0446df6c2376cd2b4aff6d51440c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dfc72c25c227d40a72b8e96260a914",
+    "url": "/GiftFinder/index.html"
+  },
+  {
+    "revision": "410d9569d32a9f8b91f0",
+    "url": "/GiftFinder/static/css/main.e11174f1.chunk.css"
+  },
+  {
+    "revision": "0b1e57520d70458f47fc",
+    "url": "/GiftFinder/static/js/2.9a3a964f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GiftFinder/static/js/2.9a3a964f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "410d9569d32a9f8b91f0",
+    "url": "/GiftFinder/static/js/main.32a2b362.chunk.js"
+  },
+  {
+    "revision": "a766c649a1f31d4b4769",
+    "url": "/GiftFinder/static/js/runtime-main.a130d94c.js"
+  },
+  {
+    "revision": "d77c9c0ebdc8565e6d5ebaede4e8270a",
+    "url": "/GiftFinder/static/media/img1.d77c9c0e.jpg"
+  },
+  {
+    "revision": "b17161a0ad4849496d767b19d04ad25e",
+    "url": "/GiftFinder/static/media/img2.b17161a0.jpg"
+  },
+  {
+    "revision": "2bfec249799437cb7995ffa9ede6c369",
+    "url": "/GiftFinder/static/media/img3.2bfec249.jpg"
+  },
+  {
+    "revision": "47f7cb0556e26e50f425ac57d3b98ee0",
+    "url": "/GiftFinder/static/media/img4.47f7cb05.jpg"
+  },
+  {
+    "revision": "1e18e08d222b039fbd287d38a941fc1a",
+    "url": "/GiftFinder/static/media/img5.1e18e08d.jpg"
+  },
+  {
+    "revision": "0293c0766d4bb8ecf38fa54e2d0f8587",
+    "url": "/GiftFinder/static/media/img6.0293c076.jpg"
+  },
+  {
+    "revision": "5f2071b4d71388f9a928ad840df14bb9",
+    "url": "/GiftFinder/static/media/img7.5f2071b4.jpg"
+  }
+]);
